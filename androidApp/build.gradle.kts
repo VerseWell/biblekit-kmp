feec5ed87199cf5e8cbf9c4d6beb6e4a61c6a727
Rahul Katariya <rahulkatariya@me.com>
2025-12-29 14:30:08 +0000

    Update Kotlin compiler configuration to use compilerOptions API

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
@@ -31,8 +33,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
     }
     buildFeatures {
         compose = true
