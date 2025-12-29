169a4ab2ca65248fecd141b0b225af28a747b10b
Rahul Katariya <rahulkatariya@me.com>
2025-12-29 13:28:44 +0000

    Use latest available version for dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
     implementation(projects.biblekit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
